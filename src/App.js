fec73540294af50e74c29cdce84d31389003df64
Kathirvel <kathirvelsivaking@gmail.com>
2024-03-14 09:42:27 +0530

    Files added successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { Num1: '', Num2: '', Result: '' };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  Add = () => {
+    var n1 = parseInt(this.state.Num1);
+    var n2 = parseInt(this.state.Num2);
+    var sum = n1 + n2;
+    this.setState({ Result: sum });
+  }
+
+  render() {
+    return (
+      // <div>
+      //   <h1 data-testid="MyHeading">Welcome to JEST</h1>
+      //   <br></br>
+      //   <b data-testid="MyBold">Hello Kathir</b>
+      // </div>
+      <div className='form container-fluid'>
+        <h1 data-testid="Heading">Addition of Two numbers</h1>
+        <br></br>
+        <label data-testid="lblNumOne" className='h3'>Enter no one</label><input data-testid="txtNumOne" type='number' value={this.state.Num1} onChange={(e) => { this.setState({ Num1: e.target.value }); }} />
+        <br></br>
+        <label data-testid="lblNumTwo" className='h3'>Enter no two</label><input data-testid="txtNumTwo" type='number' value={this.state.Num2} onChange={(e) => { this.setState({ Num2: e.target.value }); }} />
+        <br></br>
+        <input data-testid="btnSubmit" type='submit' value="Addition" className='btn btn-success' onClick={this.Add} />
+        <br></br>
+        <b data-testid="Result" className='display-1'>Sum : {this.state.Result}</b>
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,88 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+xdescribe('My Test Suite', () => {
+
+  test('Test Case-1', () => {
+    render(<App />);
+    var element1 = screen.getByTestId("MyHeading");
+    // var text1 = screen.getByText('Welcome to JEST');
+    expect(element1).toBeInTheDocument();
+    expect(element1).toHaveTextContent('Welcome to JEST');
+  });
+
+  it('Test Case-2', () => {
+    render(<App />);
+    var text2 = screen.getByText('Hello Kathir');
+    expect(text2).toBeInTheDocument();
+  });
+
+});
+
+
+//MyCalculator Test Suite
+describe("My Calculator Test Suite", () => {
+
+  test('TestCase-1[Render All UI]', () => {
+    render(<App />);
+    const txtNumOne = screen.getByTestId('txtNumOne');
+    const txtNumTwo = screen.getByTestId('txtNumTwo');
+    const btnSubmit = screen.getByTestId('btnSubmit');
+    expect(txtNumOne).toBeInTheDocument();
+    expect(txtNumTwo).toBeInTheDocument();
+    expect(btnSubmit).toBeInTheDocument();
+  });
+
+  test('Test Case-2[Render All Labels and Text Message]', () => {
+    render(<App />);
+    const heading = screen.getByTestId("Heading");
+    const lblNumOne = screen.getByTestId("lblNumOne");
+    const lblNumTwo = screen.getByTestId("lblNumTwo");
+    const Result = screen.getByTestId("Result");
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Addition of Two numbers");
+
+    expect(lblNumOne).toBeInTheDocument();
+    expect(lblNumOne).toHaveTextContent("Enter no one");
+
+    expect(lblNumTwo).toBeInTheDocument();
+    expect(lblNumTwo).toHaveTextContent("Enter no two");
+
+    expect(Result).toBeInTheDocument();
+    expect(Result).toHaveTextContent("Sum :");
+
+  });
+
+  test('Test Case-3 [Testing Textboxes Initial Value]', () => {
+    render(<App />);
+    const txtNumOne = screen.getByTestId("txtNumOne");
+    const txtNumTwo = screen.getByTestId("txtNumTwo");
+    expect(txtNumOne).toHaveTextContent('');
+    expect(txtNumTwo).toHaveTextContent('');
+  });
+
+  test('Test Case-4 [Testing Empty or Blank Input]', () => {
+    render(<App />);
+    const btnSubmit = screen.getByTestId("btnSubmit");
+    fireEvent.click(btnSubmit);
+    const Result = screen.getByTestId("Result");
+    expect(Result).toHaveTextContent("Sum : NaN");
+  });
+
+  test('Test Case-5 [Testing Success Result]', () => {
+    render(<App />);
+    const txtNumOne = screen.getByTestId("txtNumOne");
+    const txtNumTwo = screen.getByTestId("txtNumTwo");
+    const btnSubmit = screen.getByTestId("btnSubmit");
+
+    fireEvent.change(txtNumOne, { target: { value: 10 } });
+    fireEvent.change(txtNumTwo, { target: { value: 20 } });
+
+    fireEvent.click(btnSubmit);
+    const Result = screen.getByTestId("Result");
+    expect(Result).toHaveTextContent("Sum : 30");
+
+  });
+
 });
